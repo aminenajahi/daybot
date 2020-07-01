3d689588d659f3f3075b21da70b7f0f416a48501
Amine Najahi <amine.najahi@gmail.com>
2020-07-01 11:33:42 -0400

    staging changes
    Signed-off-by: Amine Najahi <amine.najahi@gmail.com>

---
+++ backtest.bat
@@ -0,0 +1,1 @@
+python.exe RunBot.py --watchlist OXY --watchlistonly True --period 5 --botname MMBot --marketdata alpha --brokertype sim  --quota 2000 --budget 25000 --backtest True
---
+++ testbot.bat
@@ -0,0 +1,1 @@
+python.exe RunBot.py --finviz 3 --watchlist AAPL --watchlistonly True --period 5 --botname MMBot --marketdata ibkr --brokertype ibkr --email True --quota 2000 --budget 50000 --backtest True
